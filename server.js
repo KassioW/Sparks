dd18a253e63073e4ec840337b114a256d1c0c0b6
KassioW <86385006+KassioW@users.noreply.github.com>
2024-11-22 16:58:58 -0300

    Atualizando
    integração com G cloud e integrando com frontend

--- server.js
+++ server.js
@@ -2,7 +2,8 @@ import express from "express";
 import routes from "./src/routes/postsRoutes.js";
 
 const app = express();
-routes(app)
+app.use(express.static("uploads"));
+routes(app);
 
 // Inicia o servidor na porta 3000
 app.listen(3000, () => {
